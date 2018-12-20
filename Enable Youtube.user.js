de965c9eb771fdd7b4e54e4cdb2b2fd067a531ea
sumitps <33196794+sumitps@users.noreply.github.com>
2018-12-20 20:30:06 +0530

    added support for youtube links clicked via google searches

--- Enable Youtube.user.js
+++ Enable Youtube.user.js
@@ -5,13 +5,20 @@
 // @description  Watch Youtube that's all
 // @author       Makarov
 // @match        https://www.youtube.com/*
+// @match        https://www.google.com/*
+// @require      http://code.jquery.com/jquery-3.3.1.min.js
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    var videoLinks = document.querySelectorAll('a[href*="/watch?v="]');
-    videoLinks.forEach(function(link){
-        link.href+='&feature=youtu.be';
+    $(document).ready(function(){
+        setTimeout(function(){
+            var videoLinks = document.querySelectorAll('a[href*="/watch?v="]');
+            videoLinks.forEach(function(link){
+                link.href+='&feature=youtu.be';
+                //console.log(link.href);
+            });
+        }, 3000);
     });
 })();
